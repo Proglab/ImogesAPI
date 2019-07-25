fa874cc74e7461a55fcfadbf154fce379c7f480d
Fabrice <info@proglab.com>
2019-07-26 00:53:23 +0200

    :building_construction: route REST

--- app/route/realties.route.js
+++ app/route/realties.route.js
@@ -3,7 +3,6 @@ const authJwt = require('./../middleware/verifyJwtToken');
 module.exports = function(app) {
     const controller = require('../controller/realties.controller.js');
 
-    app.get('/api/realties/getall', controller.getAll);
-    app.get('/api/realties/getRealtiesByProject/:id', controller.getRealtiesByProject);
+    app.get('/api/realties', controller.getAll);
 
 };
