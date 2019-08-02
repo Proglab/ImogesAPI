719ea346c4406c0153d922d287b5ccaad9159f6a
Unknown <info@absolute-fx.com>
2019-08-02 03:21:10 +0200

    :bug: Debug getOne realty

--- app/route/realties.route.js
+++ app/route/realties.route.js
@@ -5,6 +5,6 @@ module.exports = function(app) {
 
     app.get('/api/realties', controller.getAll);
     app.get('/api/projects/:id/realties', controller.getRealtiesByProject);
-    app.get('/api/projects/:projectid/realties/:id', controller.getOne);
+    app.get('/api/realties/:id', controller.getOne);
 
 };
