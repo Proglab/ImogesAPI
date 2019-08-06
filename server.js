123938cb74256361079fb97e57b3afad8db47d87
Fabrice <info@proglab.com>
2019-08-06 15:21:26 +0200

    :rocket: trying Heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app/app');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
 const db = require('./app/models');
 
 require('./app/route/index.js')(app);
