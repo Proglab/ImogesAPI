0d72c6b4b80211fed2b5b9d73c70b6367661987f
Fabrice <info@proglab.com>
2019-08-06 17:04:08 +0200

    :recycle: variables "délicates" dans le .env

---
+++ app/controller/newsletter.controller_dist.js
@@ -0,0 +1,65 @@
+
+    const mailjet = require ('node-mailjet')
+        .connect(process.env.MAILJET_KEY,process.env.MAILJET_SECRET);
+    const db = require('../models');
+    const Users = db.users;
+
+
+    function createContact(user, client)
+    {
+        console.log(user);
+        return mailjet.post("contact")
+        .request({
+            "ContactsLists":[
+                {
+                    "ListID": 3,
+                    "action": "addnoforce"
+                }
+            ],
+            "Contacts":[
+                {
+                    "Email": user.email,
+                    "Name": user.firstname + " " + user.lastname,
+                    "Properties": {
+                        "Client": client,
+                        "Lastname": user.firstname,
+                        "Firstname": user.lastname
+                    }
+                }
+            ]
+        });
+    }
+
+    // Post a Customer
+    exports.create = (req, res) => {
+        Users.findOne({ where: {email: req.body.email} }).then(user => {
+            createContact(user, true)
+            .then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            });
+        }).catch((err) => {
+            user = {};
+            user.email = req.body.email;
+            user.firstname = "";
+            user.lastname = "";
+            createContact(user, false).then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            });
+        });
+    };
+
+    // Delete a Customer by Id
+    exports.delete = (req, res) => {
+        const id = req.params.customerId;
+        Users.destroy({
+            where: { id: id }
+        }).then(() => {
+            res.status(200).send('deleted successfully a customer with id = ' + id);
+        });
+    };
