def59a311bce446f5566b84baa1a8849a0f71a57
Unknown <info@absolute-fx.com>
2019-08-20 23:29:49 +0200

    :sparkles: Cross origin sur realties

--- app/route/realties.route.js
+++ app/route/realties.route.js
@@ -1,10 +1,11 @@
 const authJwt = require('./../middleware/verifyJwtToken');
+const allowCrossOrigin = require('./../middleware/allowCrossOrigin');
 
 module.exports = function(app) {
     const controller = require('../controller/realties.controller.js');
 
-    app.get('/api/realties', controller.getAll);
-    app.get('/api/projects/:id/realties', controller.getRealtiesByProject);
-    app.get('/api/realties/:id', controller.getOne);
+    app.get('/api/realties', [allowCrossOrigin], controller.getAll);
+    app.get('/api/projects/:id/realties', [allowCrossOrigin], controller.getRealtiesByProject);
+    app.get('/api/realties/:id', [allowCrossOrigin], controller.getOne);
 
 };
